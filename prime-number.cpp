564c9deed46597ddb98b94ea88074425649958e9
neasher <neasherahmed75@gmail.com>
2022-12-06 03:38:26 +0600

    Prime number

---
+++ prime-number.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int num, i, count = 0;
+    printf("Enter any positive number : ");
+    cin >> num;
+
+    for (i = 2; i < num; i++)
+{
+
+        if (num % i == 0)
+        {
+            count++;
+            break;
+        }
+    }
+
+    if (count == 0)
+        cout << num << " is a prime number";
+
+    else
+        cout << num << " is not a prime number";
+
+    return 0;
+}
